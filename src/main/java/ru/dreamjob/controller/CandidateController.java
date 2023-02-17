9a584ab4569123c62fd0ce37f6d0ee2b39a7501d
IvanM13th <bmchek13@gmail.com>
2023-02-17 15:31:10 +0500

    6. Создание, просмотр, редактирование и удаление кандидатов [#504858]

--- src/main/java/ru/dreamjob/controller/CandidateController.java
+++ src/main/java/ru/dreamjob/controller/CandidateController.java
@@ -2,8 +2,9 @@ package ru.dreamjob.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import ru.dreamjob.model.Candidate;
+import ru.dreamjob.model.Vacancy;
 import ru.dreamjob.repository.CandidateRepository;
 import ru.dreamjob.repository.MemoryCandidateRepository;
 
@@ -21,5 +22,36 @@ public class CandidateController {
     @GetMapping("/create")
     public String getCreationPage() {
         return "candidates/create";
+    }
+
+    @GetMapping("/{id}")
+    public String getById(Model model, @PathVariable int id) {
+        var candidateOptional = candidateRepository.findById(id);
+        if (candidateOptional.isEmpty()) {
+            model.addAttribute("message", "Соискатель с таким id не найден");
+            return "errors/404";
+        }
+        model.addAttribute("candidate", candidateOptional.get());
+        return "candidates/one";
+    }
+
+    @PostMapping("/update")
+    public String update(@ModelAttribute Candidate candidate, Model model) {
+        var isUpdated = candidateRepository.update(candidate);
+        if (!isUpdated) {
+            model.addAttribute("message", "Соискатель с указанным идентификатором не найден");
+            return "errors/404";
+        }
+        return "redirect:/candidates";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(Model model, @PathVariable int id) {
+        var isDeleted = candidateRepository.deleteById(id);
+        if (!isDeleted) {
+            model.addAttribute("message", "Соискатель с указанным идентификатором не найдена");
+            return "errors/404";
+        }
+        return "redirect:/candidates";
     }
 }
